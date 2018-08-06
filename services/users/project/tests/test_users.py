b041e060edfe5a7569f7ce9222409992a4576949
Andrey Larin <larin.projects@gmail.com>
2018-08-07 00:56:21 +0300

    Added codecoverage. Fixed codestyle

--- services/users/project/tests/test_users.py
+++ services/users/project/tests/test_users.py
@@ -54,7 +54,8 @@ class TestUserService(BaseTestCase):
             self.assertIn('fail', data['status'])
 
     def test_add_user_invalid_json_keys(self):
-        """Ensure error is thrown if the JSON object does not have a username key."""
+        """Ensure error is thrown if the JSON object does
+        not have a username key."""
         with self.client:
             response = self.client.post(
                 '/users',
@@ -138,7 +139,8 @@ class TestUserService(BaseTestCase):
             self.assertIn('success', data['status'])
 
     def test_main_no_users(self):
-        """Ensure the main route behaves correctly when no users have been added to the database."""
+        """Ensure the main route behaves correctly when no
+        users have been added to the database."""
         with self.client:
             response = self.client.get('/')
             self.assertEqual(response.status_code, 200)
@@ -146,7 +148,8 @@ class TestUserService(BaseTestCase):
             self.assertIn(b'<p>No users!</p>', response.data)
 
     def test_main_with_users(self):
-        """Ensure the main route behaves correctly when users have been added to the database."""
+        """Ensure the main route behaves correctly when users
+        have been added to the database."""
         add_user('michael', 'michael@mherman.org')
         add_user('fletcher', 'fletcher@notreal.com')
         with self.client:
